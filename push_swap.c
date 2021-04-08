0f57b173120e4b900580566641e1428662dfc447
yhebbat <58018346+yhebbat@users.noreply.github.com>
2021-04-08 13:46:44 +0100

    Update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -2,34 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-/*
-void		ft_checker(t_stack *k)
-{
-	k->i = 0;
-	while (k->i < k->all)
-	{
-		//IMPORTANT
-		if (k->a[k->i++] > k->a[k->i])
-		{
-			printf("KO\n");
-			return ;
-		}
-	}
-	printf("OK\n");
-}
-
-void    sa(t_stack *l)
-{
-    int tmp;
-
-    tmp = 0;
-    if (l->all > 2)
-    {
-        tmp = l->a[l->all - 1];
-        l->a[l->all - 1] = l->a[l->all - 2];
-        l->a[l->all - 2] = tmp; 
-    }
-}*/
 
 void	ft_lstadd_first(t_stack **lst, int val)
 {
@@ -39,24 +11,88 @@ void	ft_lstadd_first(t_stack **lst, int val)
 	if(help != NULL)
 	{
 		help->value = val;
-		help->preced = *lst;
+		help->suivant = *lst;
 		*lst = help;
 	}
 }
 
-// void	ft_lstdel_first(t_stack **lst)
-// {
-// 	t_stack tmp;
-// 	if (*lst && lst)
-// 	{
-		
-// 	}
-// }
+void	ft_lstdel_first(t_stack **lst)
+{
+	t_stack *tmp;
+
+	if (*lst != NULL && lst != NULL)
+	{
+		tmp = *lst;
+		*lst = (*lst)->suivant;
+		tmp->value = 0;
+		tmp->suivant = NULL;
+		free(tmp);
+	}
+}
+
+void	add_last(t_stack **lst, int val)
+{
+	t_stack *new;
+	t_stack *help;
+
+	new = malloc(sizeof(t_stack));
+	if (new != NULL)
+	{
+		new->suivant = NULL;
+		new->value = val;
+		if(*lst == NULL)
+			*lst = new;
+		else
+		{
+			help = *lst;
+			while(help->suivant != NULL)
+				help = help->suivant;
+			help->suivant=new;
+		}
+	}
+}
+
+void		del_last(t_stack **p)
+{
+	t_stack *tmp;
+	t_stack *help;
+
+	if (p != NULL && *p != NULL)
+	{
+		if ((*p)->suivant == NULL)
+		{
+			free(*p);
+		}
+		tmp = *p;
+		help = NULL;
+		while (tmp->suivant != NULL)
+		{
+			help = tmp;
+			tmp = tmp->suivant;
+		}
+		help->suivant = NULL;
+		free(tmp);
+	}
+}
+
+t_liste		*initialisation()
+{
+	t_stack	*p;
+	t_liste *f;
+	
+	p = malloc(sizeof(t_stack));
+	f = malloc(sizeof(t_liste));
+	if (p == NULL || f == NULL)
+		return (-1);
+	p->value = 0;
+	p->suivant = NULL;
+	f->first = p;
+	return (f);
+}
 
 t_stack		*ft_newstack(int ac, char **av)
 {
-	t_stack	*p;
-	
+	t_stack *p;
 	p = NULL;
 	while (ac > 1)
 	{
@@ -69,18 +105,84 @@ t_stack		*ft_newstack(int ac, char **av)
 	return (p);
 }
 
-int			ft_checkvalue(int ac, char **av)
+int			check_overflow(char *str)
 {
-	int		i;
+	long long r;
 	
-	i = 1;
-	while (i < (ac - 1))
+	r = atoi(str);
+	if (r > 2147483647 || r < -2147483648)
+		return (0);
+	return (1);
+}
+
+int			is_digit(char *str)
+{
+	if (*(str + 1) != '\0' && (*str == '+' || *str == '-'))
+		str++;
+	while (*str)
 	{
-		if (!isdigit((int)av[i]))
+		if(*str < '0' || *str > '9')
 			return (0);
-		i++;
+		str++;
 	}
 	return (1);
+}
+
+int			ft_checkvalue(t_stack *p, char *av)
+{	
+	if (is_digit(av) || check_overflow(av) /*|| check_duplicate(p, atoi(av))*/)
+		return (0);
+	return (1);
+}
+
+void		swap(t_stack **lst)
+{
+	int		tmp;
+
+	if (lst != NULL && *lst != NULL)
+	{
+		tmp = (*lst)->value;
+		(*lst)->value = (*lst)->suivant->value;
+		(*lst)->suivant->value = tmp;
+	}
+}
+
+void		command(t_stack **a, t_stack **b, char *line)
+{
+	if (strcmp(line, "sa") == 0)
+		swap(&a);
+	else if (strcmp(line, "sb") == 0)
+		swap(&b);
+	else if (strcmp(line, "ss") == 0)
+		swap(&a);
+	else if (strcmp(line, "pa") == 0)
+		swap(&a);
+	else if (strcmp(line, "pb") == 0)
+		swap(&a);
+	else if (strcmp(line, "ra") == 0)
+		swap(&a);
+	else if (strcmp(line, "rb") == 0)
+		swap(&a);
+	else if (strcmp(line, "rr") == 0)
+		swap(&a);
+	else if (strcmp(line, "rra") == 0)
+		swap(&a);
+	else if (strcmp(line, "rrb") == 0)
+		swap(&a);
+	else if (strcmp(line, "rrr") == 0)
+		swap(&a);
+}
+
+void		parsing(t_stack **a, t_stack **b)
+{
+	char	*line;
+	while (get_next_line(0, &line))
+	{
+		if (strcmp(line, "") == 0)
+			break ;
+		command(a, b, line);
+		free(line);
+	}
 }
 
 int			main(int ac, char **av)
@@ -100,11 +202,7 @@ int			main(int ac, char **av)
 	else
 	{
 		a = ft_newstack(ac, av);
-		// if (ft_checkvalues(ac, **av))
-		// {
-		// }
-		// else
-		// 	printf("Error\n");
+		parsing(&a, &b);
 	}
 	
     /*else
