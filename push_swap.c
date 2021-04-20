683a9166738572c5c04996626a0313c4e7522a7c
Yassine Hebbat <yhebbat@e2r2p2.1337.ma>
2021-04-20 17:44:51 +0000

    to do list

--- push_swap.c
+++ push_swap.c
@@ -1,10 +1,87 @@
 #include "prj.h"
-/*
-void	ft_pushswap(t_list *head, t_list *head_b)
+
+void	ft_sortt(t_list *head)
 {
+	t_stack *a;
+
+	a = head->header;
+	if (a->value > a->suivant->value)
+	{
+		printf("sa\n");
+	}
+}
+
+void	ft_sorttt(t_list *head)
+{
+	t_stack *a;
+	
+	a = head->header;
+	if (a->value > a->suivant->value && a->suivant->value < a->suivant->suivant->value)
+	{
+		if (a->value < a->suivant->suivant->value)
+			printf("sa\n");
+		else
+			printf("ra\n");
+	}
+	if (a->value > a->suivant->value && a->suivant->value > a->suivant->suivant->value)
+		printf("sa\nrra\n");
+	if (a->value < a->suivant->value && a->suivant->value > a->suivant->suivant->value)
+	{
+		if (a->value < a->suivant->suivant->value)
+			printf("sa\nra\n");
+		else
+			printf("rra\n");
+	}
+}
+
+void	ft_sortttt(t_list *head, t_list *head_b)
+{
+	t_stack *a;
+	t_stack *b;
+	int		i;
+	int		tmp;
+
+	i = 0;
+	a = head->header;
+	b = head_b->header;
+	push(head_b, head);
+	printf("pb\n");
+	ft_sorttt(head);
+	printf("pa\n");
+	push(head_b, head);
+	tmp = a->value;
+	//if (a->value < a->suivant->value && a->value > a->suivant->suivant->value)
+	//	printf("sa\n")
+	while (a->suivant != NULL)
+	{
+		if (tmp >= a->value)
+			i++;
+		else
+			break ;
+		a = a->suivant;
+	}
+	
+	
+}
+
+void	ft_pushswap(t_list *head, t_list *head_b, int ac)
+{
+	if (ac == 3)
+		ft_sortt(head);
+	if (ac == 4)
+		ft_sorttt(head);
+	if (ac == 5)
+		ft_sortttt(head, head_b);
+	/*if (ac > 2)
+	{
+		if (ac == 3)
+		{
+			if ()
+		}
+	}*/
 
 }
-*/
+
 int main(int ac, char **av)
 {
 	t_list *header;
@@ -34,7 +111,10 @@ int main(int ac, char **av)
 			a = a->suivant;
 		}
 		//ft_checker(header);
-		// ft_pushswap(header, header_b);
+		ft_pushswap(header, header_b, ac);
+		ft_free(header);
+		ft_free(header_b);
+		exit(0);
 	}
 	else
 		ft_exit();
